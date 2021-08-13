29ad7e77c88a59220a885058bdd4872e36dbca2b
mileswang97 <mileswang@college.harvard.edu>
2021-08-13 16:31:42 -0400

    done

--- main.py
+++ main.py
@@ -4,15 +4,45 @@ expected csv file should exist in ../data/destination/ after the execution is co
 """
 # PSL
 import argparse
-import sys
+import os
 # self-defined modules
-from utils import test
+from utils import (
+    get_source_data, 
+    sort_header, 
+    write_to_csv, 
+    find_date_index, 
+    find_time_index, 
+    error_check,
+)
+from src.some_storage_library import SomeStorageLibrary
 
 if __name__ == '__main__':
     """Entrypoint"""
     print('Beginning the ETL process...')
+    
+    # add optional argument for changing delimiter, in case its not pipe
     parser = argparse.ArgumentParser()
-    parser.add_argument("delim")
+    parser.add_argument("-d")
     args = parser.parse_args()
-    delimiter = args.delim
-    #test()
+    delimiter = args.d
+
+    # define filepaths
+    sourcecolumnsfp = os.path.join(os.getcwd(), 'data', 'source', 'SOURCECOLUMNS.txt')
+    sourcedatafp = os.path.join(os.getcwd(), 'data', 'source', 'SOURCEDATA.txt')
+    sourceresultfp = 'result.csv'
+
+    # get data
+    source_headers = get_source_data(sourcecolumnsfp, delimiter)
+    source_data = get_source_data(sourcedatafp, delimiter)
+    sorted_headers_list = sort_header(source_headers)
+
+    # error check data, log errors
+    # I suspect this wasn't part of the assignment given how many lines were removed, 
+    # But I would want this in practice (less data is better than faulty data!)
+    date_index_list = find_date_index(sorted_headers_list)
+    time_index_list = find_time_index(sorted_headers_list)
+    final_data = error_check(source_data, date_index_list, time_index_list)
+
+    # write csv and move to directory
+    write_to_csv(final_data, sorted_headers_list, sourceresultfp)
+    SomeStorageLibrary().load_csv(sourceresultfp)
--- utils.py
+++ utils.py
@@ -1,22 +1,14 @@
 # PSL
-import datetime as dt
-import os
+import datetime
 import csv
 
-class Order:
-    def __init__(self):
-        pass
-    
-
-# customize delimiter
-delimiter = "|"
-
-def test():
-    print("testing")
-    return
+## GRABBING AND WRITING DATA
 
 # return list of lines, stripped and split
 def get_source_data(filepath, delimiter) -> list:
+    if delimiter is None:
+        # default delimiter
+        delimiter = '|'
     temp_list = list()
     with open(filepath, 'r') as f:
         lines = f.readlines()
@@ -25,12 +17,59 @@ def get_source_data(filepath, delimiter) -> list:
             temp_list.append(split)
     return temp_list
 
-sourcecolumns = os.path.join(os.getcwd(), "data", "source", "SOURCECOLUMNS.txt")
-sourcedata = os.path.join(os.getcwd(), "data", "source", "SOURCEDATA.txt")
+def sort_header(header_list) -> list:
+    sorted_header = sorted(header_list, key=lambda obj: int(obj[0]))
+    sorted_headers_list = [obj[1] for obj in sorted_header]
+    return sorted_headers_list
 
-print(get_source_data(sourcecolumns, "|"))
+def write_to_csv(obj_list, header, filepath) -> None:
+    with open(filepath, 'w') as f:
+        csv_writer = csv.writer(f, delimiter=',')
+        csv_writer.writerow(header)
+        for row in obj_list:
+            csv_writer.writerow(row)
+    return
 
-def write_to_open_csv(csv_writer, write_rows):
-    for row in write_rows:
-        csv_writer.writerow(row)
+## ERROR CHECKING
 
+def append_log(message, filepath='log.txt') -> None:
+    with open(filepath, 'a') as f:
+        f.write(message)
+    return
+
+def find_date_index(sorted_headers_list) -> list:
+    return ['Date' in obj for obj in sorted_headers_list]
+
+def find_time_index(sorted_headers_list) -> list:
+    return ['Time' in obj for obj in sorted_headers_list]
+
+def validate_date(str) -> bool:
+    try:
+        datetime.datetime.strptime(str, "%Y-%m-%d")
+        return True
+    except:
+        return False
+
+def validate_time(str) -> bool:
+    try:
+        datetime.datetime.strptime(str, "%H:%M:%S")
+        return True
+    except:
+        return False
+
+def error_check(obj_list, date_index_list, time_index_list):
+    append_log(f'RUNNING {datetime.datetime.today()}\n')
+    removal_list = list()
+    for obj in obj_list:
+        validation_list = list()
+        for index in range(len(obj)):
+            if date_index_list[index]:
+                validation_list.append(validate_date(obj[index]))
+            if time_index_list[index]:
+                validation_list.append(validate_time(obj[index]))
+        if False in validation_list:
+            removal_list.append(obj)        
+    for obj_remove in removal_list:
+        append_log(f'Line {obj_remove} has been removed.\n')
+        obj_list.remove(obj_remove)
+    return obj_list
